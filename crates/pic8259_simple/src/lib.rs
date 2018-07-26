e870e8479c3b2c5f2a173d96ba32cd128b6b4e55
Philipp Oppermann <dev@phil-opp.com>
2018-07-26 15:08:35 +0200

    Replace ChainedPics struct by top level functions

--- crates/pic8259_simple/src/lib.rs
+++ crates/pic8259_simple/src/lib.rs
@@ -22,7 +22,99 @@
 #![feature(const_fn)]
 #![no_std]
 
-extern crate cpuio;
+extern crate x86_64;
+
+use x86_64::instructions::port::Port;
+
+pub const TIMER_INTERRUPT: u8 = PIC_1_OFFSET;
+
+const PIC_1_OFFSET: u8 = 0x20;
+const PIC_2_OFFSET: u8 = 0x28;
+
+/// Initialize both our PICs.  We initialize them together, at the same
+/// time, because it's traditional to do so, and because I/O operations
+/// might not be instantaneous on older processors.
+pub unsafe fn initialize() {
+    let (mut pic_1, mut pic_2) = create_pic_structs();
+
+    // We need to add a delay between writes to our PICs, especially on
+    // older motherboards.  But we don't necessarily have any kind of
+    // timers yet, because most of them require interrupts.  Various
+    // older versions of Linux and other PC operating systems have
+    // worked around this by writing garbage data to port 0x80, which
+    // allegedly takes long enough to make everything work on most
+    // hardware.  Here, `wait` is a closure.
+    let mut wait_port: Port<u8> = Port::new(0x80);
+    let mut wait = || wait_port.write(0);
+
+    // Save our original interrupt masks, because I'm too lazy to
+    // figure out reasonable values.  We'll restore these when we're
+    // done.
+    let saved_mask1 = pic_1.data.read();
+    let saved_mask2 = pic_2.data.read();
+
+    // Tell each PIC that we're going to send it a three-byte
+    // initialization sequence on its data port.
+    pic_1.command.write(CMD_INIT);
+    wait();
+    pic_2.command.write(CMD_INIT);
+    wait();
+
+    // Byte 1: Set up our base offsets.
+    pic_1.data.write(pic_1.offset);
+    wait();
+    pic_2.data.write(pic_2.offset);
+    wait();
+
+    // Byte 2: Configure chaining between PIC1 and PIC2.
+    pic_1.data.write(4);
+    wait();
+    pic_2.data.write(2);
+    wait();
+
+    // Byte 3: Set our mode.
+    pic_1.data.write(MODE_8086);
+    wait();
+    pic_2.data.write(MODE_8086);
+    wait();
+
+    // Restore our saved masks.
+    pic_1.data.write(saved_mask1);
+    pic_2.data.write(saved_mask2);
+}
+
+/// Do we handle this interrupt?
+pub fn handles_interrupt(interrupt_id: u8) -> bool {
+    let (pic_1, pic_2) = create_pic_structs();
+    pic_1.handles_interrupt(interrupt_id) || pic_2.handles_interrupt(interrupt_id)
+}
+
+/// Figure out which PIC needs to know about this
+/// interrupt.  This is tricky, because all interrupts from pic 2
+/// get chained through pic 1.
+pub unsafe fn notify_end_of_interrupt(interrupt_id: u8) {
+    let (mut pic_1, mut pic_2) = create_pic_structs();
+    if pic_1.handles_interrupt(interrupt_id) || pic_2.handles_interrupt(interrupt_id) {
+        if pic_2.handles_interrupt(interrupt_id) {
+            pic_2.end_of_interrupt();
+        }
+        pic_1.end_of_interrupt();
+    }
+}
+
+fn create_pic_structs() -> (Pic, Pic) {
+    let pic_1 = Pic {
+        offset: PIC_1_OFFSET,
+        command: Port::new(0x20),
+        data: Port::new(0x21),
+    };
+    let pic_2 = Pic {
+        offset: PIC_2_OFFSET,
+        command: Port::new(0xA0),
+        data: Port::new(0xA1),
+    };
+    (pic_1, pic_2)
+}
 
 /// Command sent to begin PIC initialization.
 const CMD_INIT: u8 = 0x11;
@@ -40,115 +132,22 @@ struct Pic {
     offset: u8,
 
     /// The processor I/O port on which we send commands.
-    command: cpuio::UnsafePort<u8>,
+    command: Port<u8>,
 
     /// The processor I/O port on which we send and receive data.
-    data: cpuio::UnsafePort<u8>,
+    data: Port<u8>,
 }
 
 impl Pic {
     /// Are we in change of handling the specified interrupt?
     /// (Each PIC handles 8 interrupts.)
-    fn handles_interrupt(&self, interupt_id: u8) -> bool {
-        self.offset <= interupt_id && interupt_id < self.offset + 8
+    fn handles_interrupt(&self, interrupt_id: u8) -> bool {
+        self.offset <= interrupt_id && interrupt_id < self.offset + 8
     }
 
     /// Notify us that an interrupt has been handled and that we're ready
     /// for more.
     unsafe fn end_of_interrupt(&mut self) {
         self.command.write(CMD_END_OF_INTERRUPT);
-    }
-}
-
-/// A pair of chained PIC controllers.  This is the standard setup on x86.
-pub struct ChainedPics {
-    pics: [Pic; 2],
-}
-
-impl ChainedPics {
-    /// Create a new interface for the standard PIC1 and PIC2 controllers,
-    /// specifying the desired interrupt offsets.
-    pub const unsafe fn new(offset1: u8, offset2: u8) -> ChainedPics {
-        ChainedPics {
-            pics: [
-                Pic {
-                    offset: offset1,
-                    command: cpuio::UnsafePort::new(0x20),
-                    data: cpuio::UnsafePort::new(0x21),
-                },
-                Pic {
-                    offset: offset2,
-                    command: cpuio::UnsafePort::new(0xA0),
-                    data: cpuio::UnsafePort::new(0xA1),
-                },
-            ]
-        }
-    }
-
-    /// Initialize both our PICs.  We initialize them together, at the same
-    /// time, because it's traditional to do so, and because I/O operations
-    /// might not be instantaneous on older processors.
-    pub unsafe fn initialize(&mut self) {
-        // We need to add a delay between writes to our PICs, especially on
-        // older motherboards.  But we don't necessarily have any kind of
-        // timers yet, because most of them require interrupts.  Various
-        // older versions of Linux and other PC operating systems have
-        // worked around this by writing garbage data to port 0x80, which
-        // allegedly takes long enough to make everything work on most
-        // hardware.  Here, `wait` is a closure.
-        let mut wait_port: cpuio::Port<u8> = cpuio::Port::new(0x80);
-        let mut wait = || { wait_port.write(0) };
-
-        // Save our original interrupt masks, because I'm too lazy to
-        // figure out reasonable values.  We'll restore these when we're
-        // done.
-        let saved_mask1 = self.pics[0].data.read();
-        let saved_mask2 = self.pics[1].data.read();
-
-        // Tell each PIC that we're going to send it a three-byte
-        // initialization sequence on its data port.
-        self.pics[0].command.write(CMD_INIT);
-        wait();
-        self.pics[1].command.write(CMD_INIT);
-        wait();
-
-        // Byte 1: Set up our base offsets.
-        self.pics[0].data.write(self.pics[0].offset);
-        wait();
-        self.pics[1].data.write(self.pics[1].offset);
-        wait();
-
-        // Byte 2: Configure chaining between PIC1 and PIC2.
-        self.pics[0].data.write(4);
-        wait();
-        self.pics[1].data.write(2);
-        wait();
-
-        // Byte 3: Set our mode.
-        self.pics[0].data.write(MODE_8086);
-        wait();
-        self.pics[1].data.write(MODE_8086);
-        wait();
-
-        // Restore our saved masks.
-        self.pics[0].data.write(saved_mask1);
-        self.pics[1].data.write(saved_mask2);
-    }
-
-    /// Do we handle this interrupt?
-    pub fn handles_interrupt(&self, interrupt_id: u8) -> bool {
-        self.pics.iter().any(|p| p.handles_interrupt(interrupt_id))
-    }
-
-    /// Figure out which (if any) PICs in our chain need to know about this
-    /// interrupt.  This is tricky, because all interrupts from `pics[1]`
-    /// get chained through `pics[0]`.
-    pub unsafe fn notify_end_of_interrupt(&mut self, interrupt_id: u8) {
-        if self.handles_interrupt(interrupt_id) {
-            if self.pics[1].handles_interrupt(interrupt_id) {
-                self.pics[1].end_of_interrupt();
-            }
-            self.pics[0].end_of_interrupt();
-        }
     }
 }
